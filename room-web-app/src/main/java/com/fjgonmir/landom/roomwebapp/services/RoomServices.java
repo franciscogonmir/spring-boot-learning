8fcc851d16218870ed9ced4d6cff9f97401ce18f
fjgonmir <francisco.gonzalezmiranda@plexus.es>
2022-04-05 12:55:00 +0200

    Add Config RabbitMQ Add producer Message. Add consumer Message.

--- room-web-app/src/main/java/com/fjgonmir/landom/roomwebapp/services/RoomServices.java
+++ room-web-app/src/main/java/com/fjgonmir/landom/roomwebapp/services/RoomServices.java
@@ -2,24 +2,29 @@ package com.fjgonmir.landom.roomwebapp.services;
 
 import com.fjgonmir.landom.roomwebapp.Repository.RoomRepository;
 import com.fjgonmir.landom.roomwebapp.models.Room;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomServices {
 
     private final RoomRepository roomRepository;
 
-    public RoomServices(RoomRepository roomRepository){
+    public RoomServices(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
     }
 
     @Transactional(readOnly = true)
-    public List<Room> getAllRooms(){
+    public List<Room> getAllRooms() {
         return roomRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Room getById(long id) {
+        Optional<Room> room = roomRepository.findById(id);
+        return room.orElseGet(Room::new);
     }
 }
